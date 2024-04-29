32474eebc5e68d1e9d81b944068c57ad7ab55f69
Fady Mondy <info@3x1.io>
2024-04-29 13:21:14 +0300

    update artisan commands dark mode & rtl

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,13 @@
 <x-tomato-admin-layout>
-    <x-slot name="header">
-        Artisan Terminal
-    </x-slot>
-    <x-slot name="headerBody">
+    <x-slot:header>
+        {{ trans('tomato-artisan::global.title') }}
+    </x-slot:header>
+    <x-slot:buttons>
         <x-tomato-logout />
-    </x-slot>
+    </x-slot:buttons>
+    <x-slot:icon>
+        bx bxs-terminal
+    </x-slot:icon>
 
     <TomatoArtisan :commands="@js($commands)" route="{{url('/')}}" />
 </x-tomato-admin-layout>
